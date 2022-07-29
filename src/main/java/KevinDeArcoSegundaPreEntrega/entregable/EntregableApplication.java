2ab1e78d61c593f0b930e6763778bd9db6140fcc
Kevin Alejandro De Arco Cabezas <kealable@gmail.com>
2022-07-28 19:03:04 -0500

    creacion proyecto

---
+++ src/main/java/KevinDeArcoSegundaPreEntrega/entregable/EntregableApplication.java
@@ -0,0 +1,13 @@
+package KevinDeArcoSegundaPreEntrega.entregable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KevinDeArcoSegundaPreEntrega/entregable/EntregableApplicationTests.java
@@ -0,0 +1,13 @@
+package KevinDeArcoSegundaPreEntrega.entregable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
